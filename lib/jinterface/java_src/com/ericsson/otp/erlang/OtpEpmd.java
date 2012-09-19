f4dec9bd7ea9df637a73ee2a1a3824dcc1df253f
Vlad Dumitrescu <vladdu55@gmail.com>
2012-09-19 10:17:54 +0200

    OtpEpmd.lokupNames() may hang if network is badly configured
    On some machines with weird network configurations,
    InetAddress.getLocalHost() hangs. Searching for "localhost" works (at
    least in the cases I met). The difference is that the loopback address
    will be returned, instead of the real IP address, but for the local
    machine this should not be a problem.

--- lib/jinterface/java_src/com/ericsson/otp/erlang/OtpEpmd.java
+++ lib/jinterface/java_src/com/ericsson/otp/erlang/OtpEpmd.java
@@ -358,7 +358,7 @@ public class OtpEpmd {
     }
 
     public static String[] lookupNames() throws IOException {
-	return lookupNames(InetAddress.getLocalHost());
+       return lookupNames(InetAddress.getByName("localhost"));
     }
 
     public static String[] lookupNames(final InetAddress address)
