5c1c72550d6f511b2dc4698fd20bc8c5bfeaa1b7
José Valim <jose.valim@plataformatec.com.br>
2014-10-23 09:59:30 -0200

    Fixes a bug in cover on last expressions with empty clauses
    OTP-8188 introduced a fix for handling last expressions in
    expressions like case, try and friends. However the fix did
    not account that some of those expressions like receive may
    have no clauses (only an after clause), leading to a function
    clause error when cover compiling code with such expressions.

--- lib/tools/test/cover_SUITE_data/b.erl
+++ lib/tools/test/cover_SUITE_data/b.erl
@@ -1,5 +1,5 @@
 -module(b).
--export([start/0, loop/0]).
+-export([start/0, loop/0, wait/0]).
 
 start() ->
     spawn(?MODULE, loop, []).
@@ -12,3 +12,9 @@ loop() ->
 	stop ->
 	    done
     end.
+
+%% This checks for a bug in expressions which may have
+%% multiple clauses but has none are in the same line
+%% followed by a return value.
+wait() ->
+    receive after 1000 -> done end, ok.
